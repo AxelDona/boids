1dd802b135225e4703aa08b3f60b2aac79d5370a
Axel Dona <axel.dona@outlook.com>
2023-03-01 14:26:38 +0100

    triangles

--- src/main.cpp
+++ src/main.cpp
@@ -16,17 +16,36 @@ int main(int argc, char* argv[])
 
     // Actual app
     auto ctx = p6::Context{{.title = "Simple-p6-Setup"}};
-    ctx.maximize_window();
+
+    // Set square values
+    std::vector<glm::vec2> baseCenter(100);
+    float                  base   = 0.1;
+    float                  height = 0.2;
+    glm::vec2              rightPoint(base / 2, 0.0);
+    glm::vec2              leftPoint(-rightPoint);
+    glm::vec2              topPoint(0, height);
+    std::vector<p6::Angle> rotationValue(100);
+    for (unsigned int i = 0; i <= 100; i++) {
+        baseCenter[i] = glm::vec2(p6::random::number(-ctx.aspect_ratio() + base / 2, ctx.aspect_ratio() - base / 2), p6::random::number(-1, 1));
+    }
+
+    for (unsigned int i = 0; i <= 100; i++) {
+        rotationValue[i] = p6::random::angle();
+    }
 
     // Declare your infinite update loop.
     ctx.update = [&]() {
         ctx.background(p6::NamedColor::Blue);
-        ctx.circle(
-            p6::Center{ctx.mouse()},
-            p6::Radius{0.2f}
-        );
+        for (unsigned int i = 0; i <= 100; i++) {
+            ctx.triangle(
+                baseCenter[i] + leftPoint,
+                baseCenter[i] + rightPoint,
+                baseCenter[i] + topPoint
+            );
+        }
     };
 
+    ctx.maximize_window();
     // Should be done last. It starts the infinite loop.
     ctx.start();
 }
